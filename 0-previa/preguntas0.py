4b0cdd0db011d887026a4afe57026847ff366fe2
Alejo Razeto <95495039+AlRazeto@users.noreply.github.com>
2025-08-08 12:19:58 -0300

    Update preguntas0.py - distribuir un poco

--- 0-previa/preguntas0.py
+++ 0-previa/preguntas0.py
@@ -72,11 +72,11 @@ respuestas[(1,1,"Cajas")] = [
 
 # %%
 respuestas[(1,2,"Mentir")] = [
-0, # 0. Maxima incertidumbre (entropía)
-0.75, # 1. Minima incertidumbre (entropía)
-0, # 2. Maxima incertidumbre (entropía) dada la información disponible (restricciones)
+0.1, # 0. Maxima incertidumbre (entropía)
+0.5, # 1. Minima incertidumbre (entropía)
+0.1, # 2. Maxima incertidumbre (entropía) dada la información disponible (restricciones)
 0.25, # 3. Minima incertidumbre (entropía) dada la información disponible (restricciones)
-0, # 4. Ninguna de las anteriores
+0.05, # 4. Ninguna de las anteriores
 "",
 ]
 
@@ -128,11 +128,11 @@ respuestas[(1,3,"Universos")] = [
 # %%
 respuestas[(1,4,"Razonamiento")] = [
 0, # 0. Antes del siglo 17
-0, # 1. Siglo 17
-1, # 2. Siglo 18
-0, # 3. Siglo 19
-0, # 4. Siglo 20
-0, # 5. Siglo 21
+0.05, # 1. Siglo 17
+0.7, # 2. Siglo 18
+0.05, # 3. Siglo 19
+0.1, # 4. Siglo 20
+0.1, # 5. Siglo 21
 "suponiendo que estamos hablando del enfoque bayesiano",
 ]
 
@@ -234,7 +234,7 @@ respuestas[(1,6,"Marginal")] = [
 respuestas[(1,7,"Condicional")] = [
 0, # 0. P(Y=1|X=1): 1/2
 0, # 1. P(Y=1|X=1): 1/3
-0, # 2. P(Y=1|X=1): 2/3
+0.5, # 2. P(Y=1|X=1): 2/3
 0, # 3. P(Y=1|X=1): 1/4
 0, # 4. P(Y=1|X=1): 2/4
 0, # 5. P(Y=1|X=1): 3/4
@@ -245,7 +245,7 @@ respuestas[(1,7,"Condicional")] = [
 0, # 10. P(Y=1|X=1): 1/6
 0, # 11. P(Y=1|X=1): 2/6
 0, # 12. P(Y=1|X=1): 3/6
-1, # 13. P(Y=1|X=1): 4/6
+0.5, # 13. P(Y=1|X=1): 4/6
 0, # 14. P(Y=1|X=1): 5/6
 0, # 15. Ninguna de las anteriores
 "Es la interseccion, P(X=1, Y=1) / P(X=1), osea 0.4/0.6",
@@ -263,8 +263,8 @@ respuestas[(1,7,"Condicional")] = [
 
 # %%
 respuestas[(1,8,"Overfitting")] = [
-1, # 0. No
-0, # 1. Si
+0.9, # 0. No
+0.1, # 1. Si
 "",
 ]
 
@@ -278,8 +278,8 @@ respuestas[(1,8,"Overfitting")] = [
 
 # %%
 respuestas[(1,9,"Evaluación")] = [
-1, # 0. No
-0, # 1. Si
+0.9, # 0. No
+0.1, # 1. Si
 "se define por objetivos y prioridades, por ejemplo, un modelo que clasifica y otro que predice no manejan los mismos parametros de éxito",
 ]
 
@@ -296,11 +296,11 @@ respuestas[(1,9,"Evaluación")] = [
 
 # %%
 respuestas[(1,10,"Predicción")] = [
-0, # 0. El modelo causal correcto nunca puede ser mejor prediciendo que los complejos algoritmos de AI/ML.
-0, # 1. El modelo causal correcto a veces puede ser mejor, y a veces peor, que los complejos algoritmos de AI/ML.
-0, # 2. El modelo causal correcto nunca puede ser peor prediciendo que los complejos algoritmos de AI/ML.
-0, # 3. Los modelos causales solo explican, no predicen.
-1, # 4. Ninguna de las anteriores
+0.125, # 0. El modelo causal correcto nunca puede ser mejor prediciendo que los complejos algoritmos de AI/ML.
+0.125, # 1. El modelo causal correcto a veces puede ser mejor, y a veces peor, que los complejos algoritmos de AI/ML.
+0.125, # 2. El modelo causal correcto nunca puede ser peor prediciendo que los complejos algoritmos de AI/ML.
+0.125, # 3. Los modelos causales solo explican, no predicen.
+0.5, # 4. Ninguna de las anteriores
 "Porque son modelos de prediccion basados en enormes cantidades de datos que funcionan como cajas negras en donde no se termina de comprender la relacion causa-efecto en su toma de decisiones",
 ]
 
@@ -324,13 +324,13 @@ respuestas[(1,10,"Predicción")] = [
 # %%
 respuestas[(1,11,"Diversificación")] = [
 0, # 0. Recursos asignados a Cara: 0.0
-0, # 1. Recursos asignados a Cara: 0.1
-0, # 2. Recursos asignados a Cara: 0.2
-0.75, # 3. Recursos asignados a Cara: 0.3
-0.25, # 4. Recursos asignados a Cara: 0.4
-0, # 5. Recursos asignados a Cara: 0.5
-0, # 6. Recursos asignados a Cara: 0.6
-0, # 7. Recursos asignados a Cara: 0.7
+0.025, # 1. Recursos asignados a Cara: 0.1
+0.1, # 2. Recursos asignados a Cara: 0.2
+0.6, # 3. Recursos asignados a Cara: 0.3
+0.2, # 4. Recursos asignados a Cara: 0.4
+0.05, # 5. Recursos asignados a Cara: 0.5
+0.015, # 6. Recursos asignados a Cara: 0.6
+0.01, # 7. Recursos asignados a Cara: 0.7
 0, # 8. Recursos asignados a Cara: 0.8
 0, # 9. Recursos asignados a Cara: 0.9
 0, # 10. Recursos asignados a Cara: 1.0
@@ -347,8 +347,8 @@ respuestas[(1,11,"Diversificación")] = [
 
 # %%
 respuestas[(1,12,"Apuesta individual")] = [
-0, # 0. No
-1, # 1. Sí
+0.1, # 0. No
+0.9, # 1. Sí
 "dada la esperanza es probable que aumentemos el monto aunque sea de forma muy leve y en caso de haber perdidas tambien serian pocas",
 ]
 
@@ -364,9 +364,9 @@ respuestas[(1,12,"Apuesta individual")] = [
 
 # %%
 respuestas[(1,13,"Fondo común")] = [
-0, # 0. Empeora
-0, # 1. Neutro
-1, # 1. Mejora
+0.1, # 0. Empeora
+0.1, # 1. Neutro
+0.8, # 1. Mejora
 "mejora porque la esperanza es levemente superior a 1 por lo que al haber 100 casos se puee decir que el riesgo a tener perdidas disminuye notablemnte ya que el pozo de c/u tiende a aumentar",
 ]
 
@@ -382,9 +382,9 @@ respuestas[(1,13,"Fondo común")] = [
 
 # %%
 respuestas[(1,14,"Tragedia de los comunes")] = [
-0, # 0. Nos conviene aportar al fondo común
-1, # 1. Es indistinto
-0, # 1. Nos conviene dejar de aportar mientras seguimos recibiendo la cuota del fondo común
+0.1, # 0. Nos conviene aportar al fondo común
+0.8, # 1. Es indistinto
+0.1, # 1. Nos conviene dejar de aportar mientras seguimos recibiendo la cuota del fondo común
 "lo unico que aumentaria seria el monto, no la cantidad de tiradas o el resultado de estas",
 ]
 
